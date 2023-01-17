626621c581e64a82b9f6ffb972c97d98d2502a32
ProjectsWithZoe <118231395+ProjectsWithZoe@users.noreply.github.com>
2023-01-17 19:40:12 +0000

    yaml document  modified

--- database_utils.py
+++ database_utils.py
@@ -1,3 +1,9 @@
-class DatabaseConnector:
-    def __init__(self) -> None:
-        pass
+import yaml
+
+def read_db_creds():
+    with open('db_creds.yaml') as file:
+        data = yaml.load(file, Loader=yaml.FullLoader)
+        return data
+
+data = read_db_creds()
+print(data)
