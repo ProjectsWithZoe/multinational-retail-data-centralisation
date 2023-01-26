6f824d16f056d814477ed46ad3a0b863da261d0c
ProjectsWithZoe <118231395+ProjectsWithZoe@users.noreply.github.com>
2023-01-26 14:53:47 +0000

    Database utils fixed

--- database_utils.py
+++ database_utils.py
@@ -17,27 +17,9 @@ class DatabaseConnector:
 
     def init_db_engine(self,creds_file):
         data = self.read_db_creds(creds_file)
-        connection_string = f"postgresql+{DBAPI}://{data['RDS_USER']}:{data['RDS_PASSWORD']}@{data['RDS_HOST']}:{data['RDS_PORT']}/{data['RDS_DATABASE']}"
-        engine = create_engine(connection_string)  
+        engine = create_engine(f"postgresql+{DBAPI}://{data['RDS_USER']}:{data['RDS_PASSWORD']}@{data['RDS_HOST']}:{data['RDS_PORT']}/{data['RDS_DATABASE']}")
         print(engine)
         return engine  
 
-    def list_db_tables(self,engine):
-        inspector = inspect(engine)
-        table_names = inspector.get_table_names()
-        print(table_names)
 
-    def read_data(self, engine, table_name):
-        with engine.connect() as connect:
-            cursor = connect.connection.cursor()
-            cursor.execute(f"SELECT * FROM {table_name}")
-            info = cursor.fetchall()
-            print(info)
-            return info
-
-    #engine = create_engine(f'postgresql://{RDS_USER}:{RDS_PASSWORD}@{RDS_HOST}:{RDS_PORT}/{RDS_DATABASE}')
-
-
-db1 = DatabaseConnector()
-db1.list_db_tables(db1.init_db_engine(creds_file='db_creds.yaml'))
 
