79fd93e036f7eb4819819a6a7dcedb9d98dd0b3c
ProjectsWithZoe <118231395+ProjectsWithZoe@users.noreply.github.com>
2023-01-31 21:43:32 +0000

    Data cleaned

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,4 +1,5 @@
 import data_extraction, database_utils
+import pandas as pd
 
 
 class DataCleaning:
@@ -7,8 +8,30 @@ class DataCleaning:
 
     def clean_user_data(self,database_extractor):
         user_df=database_extractor.read_rds_table(database_utils.db_connector,'legacy_users' )
-        #look out for null values
-        print(user_df.duplicated().sum())
+        #print(user_df)
+        #country_code to category
+        user_df['country_code'] = user_df['country_code'].astype('category')
+        #look out for duplicated values
+        user_df.duplicated().sum()
+        user_df.isnull().sum()
+        #look for unknown values
+        #print(user_df.sort_values(by=['company']))
+
+        #values where country code is not 2 letters
+        not_2 = user_df['country_code'].apply(lambda x: len(x) !=2)
+        new_df = user_df.drop(user_df[not_2].index, inplace=False)
+        #print(new_df)
+
+        #date values fixed
+        new_df['join_date'] = pd.to_datetime(new_df['join_date'], infer_datetime_format=True)
+        new_df['date_of_birth'] = pd.to_datetime(new_df['date_of_birth'], infer_datetime_format=True)
+        
+        
+        letters = new_df[new_df['phone_number'].str.isalpha()==True]
+        print(new_df[letters])
+
+
+        #print(user_df)
 
 
         
