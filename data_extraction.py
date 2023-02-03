fa8c725a1f07bd3a4eb5e440ea9340265f0b981e
ProjectsWithZoe <118231395+ProjectsWithZoe@users.noreply.github.com>
2023-02-03 18:57:05 +0000

    Data extraction class fixed

--- data_extraction.py
+++ data_extraction.py
@@ -35,12 +35,10 @@ class DataExtractor:
         return store_number
 
     def retrieve_stores_data(self, url, header):
-        header = {'x-api-key': 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
-        url = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_number}'
 
         response = requests.get(url,headers=header)
-        r = response.raise_for_status()
-        print (r)
+        data = response.json()
+        print(data)
         #data = response.json()
         #print(data)
 
