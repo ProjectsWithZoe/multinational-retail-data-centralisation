328cb6f9441d9c7c1af5ebf3d3a761bf6997d96c
ProjectsWithZoe <118231395+ProjectsWithZoe@users.noreply.github.com>
2023-02-03 00:35:01 +0000

    Orders table retrieved and cleaned

--- data_cleaning.py
+++ data_cleaning.py
@@ -28,6 +28,19 @@ class DataCleaning:
         
         print(new_df)
         return new_df
+    #to be updated once link is fixed
+    #def clean_store_data(self, database_extractor):
+        
+        #store_df = database_extractor.retrieve_stores_data(url,header)
+
+    def clean_orders_data(self,database_extractor):
+        orders_df = database_extractor.read_rds_table(database_utils.db_connector, 'orders_table')
+        orders_df_copy = orders_df
+
+        new_df = orders_df_copy.drop(columns=['1','first_name', 'last_name'])
+        print(new_df)
+        return new_df
+
 
 
 data_cleaner = DataCleaning()
--- data_extraction.py
+++ data_extraction.py
@@ -4,6 +4,8 @@ import psycopg2
 import pandas as pd
 import json, requests
 
+
+
 class DataExtractor:
     def __init__(self) -> None:
         pass
@@ -18,15 +20,32 @@ class DataExtractor:
         engine = db_connector.init_db_engine('db_creds.yaml')
         query = (f'SELECT * FROM {table_name}')
         df = pd.read_sql_query(query, engine )
-        #print(df)
+        print(df)
         return df
 
     def list_number_of_stores(self,url,header):
-        url = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores'
         header = {'x-api-key': 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
+        url = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores'
+        
         response = requests.get(url, headers=header)
+        r = response.raise_for_status()
+        #print(r)
+        store_number = response.json()
+        print(store_number)
+        return store_number
+
+    def retrieve_stores_data(self, url, header):
+        header = {'x-api-key': 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
+        url = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_number}'
+
+        response = requests.get(url,headers=header)
         data = response.json()
         print(data)
+
+
+
+
+
 
 
 
